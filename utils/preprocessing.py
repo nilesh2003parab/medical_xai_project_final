b3f5639c41f879bfe185a34d882db7b71322d0d3
TRENDY NILESH <95012517+nilesh2003parab@users.noreply.github.com>
2026-02-27 11:36:41 +0530

    Refactor normalization to single line

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -5,8 +5,6 @@ def get_transform():
     return transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
     ])
